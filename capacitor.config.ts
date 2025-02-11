bc5ee0652e2523314efc0fa25fab5eb83104e706
caleosilva <caleosilva75@gmail.com>
2025-02-11 13:44:30 -0300

    feat: android app via capacitorjs

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'FSY2025',
+  webDir: 'out'
+};
+
+export default config;
